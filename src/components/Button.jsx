8b9fe4743b98a8a8dce052c19985cd889536c6b4
Sara Salzano <sarasalzano95@gmail.com>
2025-10-08 17:24:45 +0200

    refactoring

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,14 @@
 import { useContext } from "react";
-import { BudgetProvider, default as BudgetContext } from "../contexts/BudgetContext";
+import { BudgetContext } from "../contexts/BudgetContext";
 
 
 export default function Button(){
 //prendi lo stato budgetMode e la funzione per aggiornarlo
-const {budgetMode, SetBudgetMode} = useContext(BudgetContext);
+const {budgetMode, setBudgetMode} = useContext(BudgetContext);
 
 return(
     //al click invertiamo il valore di budgetMode
-    <button onClick={()=>SetBudgetMode(!budgetMode)}>
+    <button onClick={()=>setBudgetMode(!budgetMode)}>
         {/* modifica testo bottone a seconda dello stato true o false del budgetmode */}
         {budgetMode ? "DEACTIVATE BUDGET MODE" : "ACTIVATE BUDGETMODE"}
     </button>
