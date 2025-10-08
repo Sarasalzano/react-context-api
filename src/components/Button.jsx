94b0b4689769307f949adaf41d00cc7513b198d1
Sara Salzano <sarasalzano95@gmail.com>
2025-10-08 17:13:10 +0200

    inversione  valore budgetmode al click bottone

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,3 +1,16 @@
+import { useContext } from "react";
+import { BudgetProvider, BudgetContext } from "../contexts/BudgetContext";
+
+
 export default function Button(){
-    return()
+//prendi lo stato budgetMode e la funzione per aggiornarlo
+const {budgetMode, SetBudgetMode} = useContext(BudgetContext);
+
+return(
+    // al click, invertiamo il valore di budgetMode
+    <button onClick={()=>SetBudgetMode(!budgetMode)}>
+        
+
+    </button>
+)
 }
