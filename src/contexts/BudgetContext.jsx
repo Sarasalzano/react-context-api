dce7ff1290d31df32f67638b93b0592131b88e91
Sara Salzano <sarasalzano95@gmail.com>
2025-10-08 16:40:38 +0200

    esegui budgetcontext

--- src/contexts/BudgetContext.jsx
+++ src/contexts/BudgetContext.jsx
@@ -1,6 +1,19 @@
-import { createContext } from 'react';
-
+import { createContext, useState } from 'react';
+//contesto per lo stato del budget
 const BudgetContext = createContext();
-
 export default BudgetContext;
 
+//componente BudgetProvider che rende visibile lo stato del budget a tutti i figli
+export function BudgetProvider({children}){
+    //variabile di stato del budget 
+    const [budgetMode, setBudgetMode] = useState(false);
+    //oggetto che contiene lo stato del budget e la funzione per modificarlo
+    const value = {budgetMode, setBudgetMode};
+
+    //fornisci contesto ai figli
+    return (
+        <BudgetContext.Provider value = {value}>
+        {children}
+        </BudgetContext.Provider>
+    )
+}
